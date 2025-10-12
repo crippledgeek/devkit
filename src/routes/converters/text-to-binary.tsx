e67ba1593e42a9f2e0682deabb28e9a0c43074f5
Mattias Carlsson <mattias.carlsson01@gmail.com>
2025-10-12 20:30:06 +0200

    Simplify loader logic in text-to-binary route.

--- src/routes/converters/text-to-binary.tsx
+++ src/routes/converters/text-to-binary.tsx
@@ -6,8 +6,8 @@ import { iconvReadyQueryOptions } from '@/lib/queries/iconv'
 export const Route = createFileRoute('/converters/text-to-binary')({
     loader: async ({ context }) => {
         // Preload the iconv-lite chunk before rendering the page
-        await context.queryClient.ensureQueryData(iconvReadyQueryOptions)
-        return null
+        return await context.queryClient.ensureQueryData(iconvReadyQueryOptions)
+
     },
     component: TextToBinary,
 })
